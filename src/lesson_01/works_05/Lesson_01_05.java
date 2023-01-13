fe5672f34ce0965d0ac1699ecd0b5d95ff0edd4c
Tomomichi Sato <tsato@dcworks.jp>
2023-01-13 21:09:35 +0900

    EngineersGate オリジナル課題 Java関連

---
+++ src/lesson_01/works_05/Lesson_01_05.java
@@ -0,0 +1,36 @@
+package lesson_01.works_05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * １．基礎構文（変数、for文、if文）
+ * <p>
+ * 5. キーに「1から5」、値に「りんご、いちご、みかん、バナナ、メロン」を順にセットした<br>
+ * HashMapを作成し、キーと値を一つずつ表示してください。
+ *
+ * @author tomo-sato
+ */
+public class Lesson_01_05 {
+
+	/**
+	 * main関数
+	 *
+	 * @param args 起動引数
+	 */
+	public static void main(String[] args) {
+		Map<Integer, String> map = new HashMap<Integer, String>() {
+			{
+				put(1, "りんご");
+				put(2, "いちご");
+				put(3, "みかん");
+				put(4, "バナナ");
+				put(5, "メロン");
+			}
+		};
+
+		for (Map.Entry<Integer, String> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
+	}
+}
