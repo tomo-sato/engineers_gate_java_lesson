c3594bd7253509ddcc5f7e2b3f6b61e0202e5e4a
Tomomichi Sato <tsato@dcworks.jp>
2023-01-13 20:37:12 +0900

    EngineersGate オリジナル課題 Java関連

---
+++ src/lesson_01/works_04/Lesson_01_04.java
@@ -0,0 +1,31 @@
+package lesson_01.works_04;
+
+/**
+ * １．基礎構文（変数、for文、if文）
+ * <p>
+ * 4. 1から100までの要素を持つ配列を作成し偶数だけ表示してください。
+ *
+ * @author tomo-sato
+ */
+public class Lesson_01_04 {
+
+	/**
+	 * main関数
+	 *
+	 * @param args 起動引数
+	 */
+	public static void main(String[] args) {
+
+		int[] iArr = new int[100];
+
+		for (int i = 0; i < 100; i++) {
+			iArr[i] = i + 1;
+		}
+		
+		for (int i : iArr) {
+			if ((i % 2) == 0) {
+				System.out.print(i + ", ");
+			}
+		}
+	}
+}
